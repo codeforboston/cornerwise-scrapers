0de5ddc82c87bec88cafcb3c25966b245f85d2a6
Brian Sanders <bdougsand+cfb@gmail.com>
2017-09-26 22:40:22 -0400

    First version

---
+++ cloud.py
@@ -0,0 +1,199 @@
+from datetime import datetime
+import glob
+import json
+import logging
+import os
+import sys
+import traceback
+
+from urllib.parse import parse_qsl
+
+logger = logging.getLogger(__name__)
+
+
+try:
+    # Add zipped packages to the same directory where this file lives:
+    zip_paths = glob.glob(os.path.join(os.path.dirname(__file__), "*.zip"))
+    sys.path += zip_paths
+except:
+    pass
+
+
+class Request(object):
+    def __init__(self,
+                 method="GET",
+                 headers=None,
+                 query=None,
+                 path="/",
+                 body=None,
+                 bodytext=None):
+        self._headers = headers or {}
+        self._query = query or {}
+        self._post_params = self._process_post_body()
+        self._path = path
+        self._bodytext = bodytext
+        self._body = body
+        self._method = method
+
+    @property
+    def content_type(self):
+        self._headers.get("content-type", "application/x-www-form-urlencoded")
+
+    def read_body(self):
+        if not self._bodytext:
+            self._bodytext = self._body.read()
+        return self._bodytext
+
+    def _process_post_body(self):
+        if self.content_type == "application/x-www-form-urlencoded":
+            return dict(parse_qsl(self.read_body()))
+
+        return {}
+
+    @property
+    def headers(self):
+        return self._headers
+
+    @property
+    def GET(self):
+        return self._query
+
+    @property
+    def POST(self):
+        return self._post_params
+
+    def __getitem__(self, k):
+        return self._post_params.get(k) or self._query.get(k)
+
+
+def make_azure_request(env):
+    headers = {}
+    query = {}
+    for key, val in env.items():
+        if key.startswith("REQ_HEADERS"):
+            headers[key[12:].lower()] = val
+        elif key.startswith("REQ_QUERY"):
+            query[key[10:].lower()] = val
+
+    inpath = env.get("req")
+    infile = open(inpath, "r") if inpath else None
+
+    return Request(
+        method=env["REQ_METHOD"].upper(),
+        headers=headers,
+        query=query,
+        path=env["REQ_HEADERS_X-ORIGINAL-URL"],
+        body=infile,)
+
+
+def json_serialize(x):
+    if isinstance(x, datetime):
+        return x.isoformat()
+
+    return str(x)
+
+
+def output(req, resp):
+    with open(os.environ["RES"]) as out:
+        out.write(json.dumps(resp, default=json_serialize))
+
+
+def body_output(req, s, status=200, content_type="text/plain"):
+    output(req, {
+        "status": status,
+        "body": s,
+        "headers": {
+            "content-type": content_type
+        }
+    })
+
+
+def redirect_output(req, location):
+    output(req, {"status": 302, "headers": {"location": location}})
+
+
+def redirect(location):
+    return lambda req: redirect_output(req, location)
+
+
+def make_lambda_request(event, context):
+    return Request(
+        method=event["httpMethod"],
+        headers=event["headers"],
+        query=event["queryStringParameters"],
+        path=event["path"],
+        body=event["body"])
+
+
+def lambda_response(body, status=200, content_type="text/plain"):
+    return {
+        "statusCode": status,
+        "body": body,
+        "headers": {
+            "content-type": content_type
+        }
+    }
+
+
+def azure(fn):
+    """
+    Function decorator that calls fn immediately with environment variables
+    processed into a Request object.
+    """
+    req = make_azure_request(os.environ)
+    try:
+        response = fn(req)
+
+        if isinstance(response, str):
+            body_output(req, response)
+        elif isinstance(response, dict):
+            body_output(req, response, content_type="application/json")
+        elif callable(response):
+            response(req)
+    except Exception as exc:
+        body_output(req, traceback.format_exc(), status=500)
+
+    return fn
+
+
+def aws_lambda(fn):
+    """Function decorator for a handler function running on AWS Lambda.
+
+    :param fn: function taking a request object
+    """
+    handler_name = f"{fn.__module__}.{fn.__name__}"
+    def do_run(event, context):
+        req = make_lambda_request(event, context)
+
+        try:
+            response = fn(req)
+        except Exception as exc:
+            logger.exception(f"Exception thrown in handler: {handler_name}")
+
+            return lambda_response(
+                {"error": f"Exception in {handler_name}"},
+                status=500,
+                content_type="application/json"
+            )
+
+        if isinstance(response, str):
+            return lambda_response(response)
+        if isinstance(response, dict):
+            return lambda_response(
+                json.dumps(
+                    response, default=str),
+                content_type="application/json")
+        if callable(response):
+            return response(req)
+
+    do_run.__name__ = fn.__name__
+    do_run.__module__ = fn.__module__
+    return do_run
+
+
+def cloud_fn(fn):
+    if "AWS_REGION" in os.environ:
+        return aws_lambda(fn)
+    # TODO: Improve testing for whether we're in Azure.
+    if True:
+        return azure(fn)
---
+++ somervillema.py
@@ -0,0 +1,366 @@
+from os import path
+import sys
+
+from datetime import datetime, timedelta
+import logging
+import re
+import pytz
+from functools import partial
+from bs4 import BeautifulSoup
+from itertools import takewhile
+import pytz
+
+from cloud import aws_lambda
+
+from urllib.request import urlopen
+from urllib.error import HTTPError, URLError
+from urllib.parse import urljoin
+
+
+logger = logging.getLogger(__name__)
+TIMEZONE = pytz.timezone("US/Eastern")
+URL_HOST = "https://www.somervillema.gov"
+URL_BASE = (f"{URL_HOST}/departments/ospcd/"
+            "planning-and-zoning/reports-and-decisions/robots")
+URL_FORMAT = URL_BASE + "?page={:1}"
+
+HEARING_HOUR = 18
+HEARING_MIN = 0
+
+def to_under(s):
+    "Converts a whitespace-separated string to underscore-separated."
+    return re.sub(r"\s+", "_", s.lower())
+
+
+def link_info(a, base=URL_BASE):
+    return {"title": a.get_text().strip(),
+            "url": urljoin(base, a["href"])}
+
+
+def get_date(d):
+    return datetime.strptime(d, "%b %d, %Y")
+
+
+def get_datetime(datestring, tzinfo=None):
+    dt = datetime.strptime(datestring, "%m/%d/%Y - %I:%M%p")
+    if tzinfo:
+        return tzinfo.localize(dt)
+
+    return dt
+
+
+def get_links(elt):
+    "Return information about the <a> element descendants of elt."
+    return [link_info(a) for a in elt.find_all("a") if a["href"]]
+
+
+# Field processors:
+def dates_field(td):
+    return get_date(default_field(td))
+
+
+def datetime_field(td, tzinfo=None):
+    return get_datetime(default_field(td), tzinfo)
+
+
+def datetime_field_tz(tz):
+    if isinstance(tz, str):
+        tz = pytz.timezone(tz)
+    return partial(datetime_field, tzinfo=tz)
+
+
+def links_field(td):
+    links = get_links(td)
+    if links:
+        return {"links": get_links(td)}
+
+
+def default_field(td):
+    return td.get_text().strip()
+
+
+def optional(fn, default=None):
+    def helper(td):
+        try:
+            val = fn(td)
+            return default if value is None else val
+        except:
+            return default
+
+    return helper
+
+
+def get_td_value(td, attr=None, processors={}):
+    processor = processors.get(attr, default_field)
+    return processor(td)
+
+
+def get_row_vals(attrs, tr, processors={}):
+    return {
+        attr: get_td_value(
+            td, attr, processors=processors)
+        for attr, td in zip(attrs, tr.find_all("td"))
+    }
+
+
+def col_names(table):
+    tr = table.select("thead > tr")[0]
+    return [th.get_text().strip() for th in tr.find_all("th")]
+
+
+def find_table(doc):
+    return doc.select("table.views-table")[0]
+
+
+def get_data(doc, get_attribute=to_under, processors={}):
+    table = find_table(doc)
+    titles = col_names(table)
+    attributes = [get_attribute(t) for t in titles]
+
+    trs = table.find("tbody").find_all("tr")
+    for i, tr in enumerate(trs):
+        yield get_row_vals(attributes, tr, processors)
+
+
+def event_title_for_case_number(case_number):
+    if case_number.startswith("PB"):
+        return "Planning Board"
+
+    if case_number.startswith("ZBA"):
+        return "Zoning Board of Appeals"
+
+
+DEFAULT_EVENT_DESCRIPTIONS = {
+    "Zoning Board of Appeals": ("The ZBA is the Special Permit Granting Authority for variances; appeals of decisions; Comprehensive Permit Petitions; and some Special Permit applications"),
+
+    "Planning Board": ("The Planning Board is the Special Permit Granting Authority for special districts and makes recommendations to the Board of Aldermen on zoning amendments.")
+}
+
+
+
+
+# Give the attributes a custom name:
+TITLES = {}
+
+
+def attribute_for_title(title):
+    """
+    Convert the title (e.g., in a <th></th>) to its corresponding
+    attribute in the output maps.
+    """
+    return TITLES.get(title, to_under(title))
+
+
+# TODO: Return None or error if the response is not successful
+def get_page(page=1, url_format=URL_FORMAT):
+    "Returns the HTML content of the given Reports and Decisions page."
+    url = url_format.format(page)
+    f = urlopen(url)
+    logger.info("Fetching page %i", page)
+    html = f.read()
+    f.close()
+    return html
+
+
+def detect_last_page(doc):
+    anchor = doc.select("li.pager-last a")[0]
+    m = re.search(r"[?&]page=(\d+)", anchor["href"])
+
+    if m:
+        return int(m.group(1))
+
+    return 0
+
+
+# Field
+
+def get_links(elt, base=URL_BASE):
+    "Return information about the <a> element descendants of elt."
+    return [link_info(a, base) for a in elt.find_all("a") if a["href"]]
+
+
+def staff_report_field(td, base=URL_BASE):
+    links = sum((a.find_all("a") for a in td.find_all("a")), [])
+    return links and {"links": [link_info(a, base) for a in links]}
+
+
+def default_field(td):
+    return td.get_text().strip()
+
+field_processors = {
+    "reports": staff_report_field,
+    "decisions": links_field,
+    "other": links_field,
+    "first_hearing_date": optional(dates_field),
+    "updated_date": datetime_field_tz(TIMEZONE)
+}
+
+
+def get_td_val(td, attr=None):
+    processor = field_processors.get(attr, default_field)
+    return processor(td)
+
+
+def parse_addresses(number, street):
+    number_sublists = re.split(r"\s*/\s*", number)
+    street_sublists = re.split(r"\s*/\s*", street)
+
+    for number, street in zip(number_sublists, street_sublists):
+        number_range_match = re.match(r"(\d+)-(\d+)", number)
+
+        if number_range_match:
+            yield (number_range_match.group(1), street)
+            yield (number_range_match.group(2), street)
+        else:
+            number_sublist = re.split(r",? and |,? & |, ", number)
+            for number in number_sublist:
+                yield (number, street)
+
+
+def get_address_list(number, street):
+    return ["{} {}".format(n, s) for (n, s) in parse_addresses(number, street)]
+
+
+def find_cases(doc):
+    """Takes a BeautifulSoup document, returns a list of maps representing the
+    proposals found in the document.
+    """
+    table = find_table(doc)
+    attributes = [attribute_for_title(t) for t in col_names(table)]
+
+    tbody = table.find("tbody")
+    trs = tbody.find_all("tr")
+
+    cases = []
+
+    for i, tr in enumerate(trs):
+        try:
+            proposal = get_row_vals(attributes, tr,
+                                    processors=field_processors)
+            addresses = get_address_list(proposal["number"], proposal["street"])
+            proposal["address"] = addresses[0]
+            proposal["all_addresses"] = addresses
+
+            # Event:
+            events = []
+            event_title = event_title_for_case_number(proposal["case_number"]),
+            event_description = DEFAULT_EVENT_DESCRIPTIONS.get(event_title)
+            first_hearing = proposal.get("first_hearing_date")
+            if first_hearing and event_title:
+                first_hearing = first_hearing.replace(hour=HEARING_HOUR, minute=HEARING_MIN)
+                events.append(
+                    {"title": event_title,
+                     "description": event_description,
+                     "date": first_hearing,
+                     "region_name": "Somerville, MA"})
+
+            # For now, we assume that if there are one or more documents
+            # linked in the 'decision' page, the proposal is 'complete'.
+            # Note that we don't have insight into whether the proposal was
+            # approved!
+            proposal["complete"] = bool(proposal["decisions"])
+            cases.append(proposal)
+        except Exception as err:
+            tr_string = " | ".join(tr.stripped_strings)
+            logger.error("Failed to scrape row %i: %s", i, tr_string)
+            logger.error(err)
+            continue
+    return cases
+
+
+def get_proposals_for_page(page):
+    html = get_page(page)
+    doc = BeautifulSoup(html, "html.parser")
+    logger.info("Scraping page {num}".format(num=page))
+    cases = find_cases(doc)
+
+    return cases
+
+
+def get_pages():
+    """Returns a generator that retrieves Reports and Decisions pages and
+    parses them as HTML.
+
+    """
+    i = 0
+    last_page = None
+
+    while True:
+        try:
+            # There's currently a bug in the Reports and Decisions page
+            # that causes nonexistent pages to load page 1. They should
+            # return a 404 error instead!
+            html = get_page(i)
+        except HTTPError as err:
+            break
+
+        except URLError as err:
+            logger.warn("Failed to retrieve URL for page %d.", i,
+                        err)
+            break
+
+        doc = BeautifulSoup(html, "html.parser")
+        if last_page is None:
+            last_page = detect_last_page(doc)
+
+        yield doc
+
+        i += 1
+
+        if i > last_page:
+            break
+
+
+def get_cases(gen=None):
+    "Returns a generator that produces cases."
+    if not gen:
+        gen = get_pages()
+
+    for doc in gen:
+        for case in find_cases(doc):
+            yield case
+
+
+def get_proposals_since(dt=None,
+                        stop_at_case=None,
+                        date_column="updated_date",):
+    """Page through the Reports and Decisions page, scraping the proposals
+    until the submission date is less than or equal to the given date.
+
+    :param dt: If provided, stop scraping when the `date_column` is less
+    than or equal to this datetime.
+
+    :param stop_at_case: If provided, stop scraping when a case number
+    matches this string
+
+    :param date_column: Customize the name of the date column
+
+    :returns: A list of dicts representing scraped cases.
+
+    """
+    if not dt.tzinfo:
+        dt = TIMEZONE.localize(dt)
+
+    def guard(case):
+        return (not dt or case[date_column] > dt) and \
+            (not stop_at_case or case["case_number"] != stop_at_case)
+
+    all_cases = list(takewhile(guard, get_cases()))
+
+    return all_cases
+
+
+@aws_lambda
+def scrape(req):
+    since = req.GET.get("since")
+    since_stamp = req.GET.get("since_stamp")
+    if since:
+        since = datetime.strptime(since, "%Y%m%d")
+    elif since_stamp:
+        since = datetime.fromtimestamp(since_stamp)
+    else:
+        now = pytz.utc.localize(datetime.utcnow()).astimezone(TIMEZONE)
+        since = now - timedelta(days=30)
+
+    cases = get_proposals_since(since)
+    return {"cases": cases}
