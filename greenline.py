39c64fd0b032a1b1f261a5e7e0bd846882d59b56
Brian Sanders <bdougsand+cfb@gmail.com>
2018-02-19 18:09:47 -0500

    Fixes to the GLX event scraper

--- greenline.py
+++ greenline.py
@@ -1,11 +1,12 @@
 """Scrapes the official Green Line Extension site to find the posting for the
 next scheduled meeting.
 """
-from datetime import datetime, time
+from datetime import datetime, time, timedelta
 import re
 from urllib.request import urlopen
 
 from bs4 import BeautifulSoup, NavigableString
+from dateutil.parser import parse
 import pytz
 
 from cloud import aws_lambda
@@ -21,7 +22,7 @@ def get_doc():
 def find_text_elem(elem, regex, tags={"tr"}):
     regex = re.compile(regex)
     def finder(desc_elem):
-        return bool(regex.match(desc_elem.text.strip())) and desc_elem.name in tags
+        return regex.match(desc_elem.text.strip()) and desc_elem.name in tags
     return elem.find(finder)
 
 
@@ -39,40 +40,68 @@ def time_match(time_range):
             time(end_hour + end_offset, end_minute))
 
 
+time_range_pattern = re.compile(
+    r"(\d\d?)(?::(\d\d))?((?: to | *- *)(\d\d?)(?::(\d\d))?)?(?: *([ap]\.?m\.?))", re.I)
+
+def match_time_range(text):
+    match = time_range_pattern.search(text)
+    if match:
+        start_time = time(int(match.group(1)),
+                          int(match.group(2) or "0"))
+
+        end_time = None
+        if match.group(3):
+            end_time = time(int(match.group(4)),
+                            int(match.group(5) or "0"))
+
+        return start_time, end_time
+
+    return None, None
+
+
+def process_event(elt):
+    lines = [d.strip() for d in elt.descendants
+             if isinstance(d, NavigableString) and d.strip()]
+    date = parse(lines[1])
+
+    start_time, end_time = match_time_range(lines[2])
+
+    start_datetime = TIMEZONE.localize(datetime.combine(date, start_time))
+    try:
+        end_datetime = TIMEZONE.localize(datetime.combine(date, end_time))
+        duration = end_datetime - start_datetime
+        hours = int(duration.total_seconds()/3600)
+        minutes = int((duration.total_seconds % 3600)/60)
+    except TypeError:
+        hours, minutes = 1, 30
+
+    locale_match = re.match(r"(.*), ([A-Z]{2}),? (\d{5})", lines[-1])
+
+    return {
+        "title": lines[0],
+        "description": "",
+        "start": start_datetime.isoformat(),
+        "duration": f"{hours}:{minutes:0>2}",
+        "region_name": "Somerville, MA",
+        "address": {
+            "name": ", ".join(map(str.strip, lines[3:-2])),
+            "street_address": lines[-2],
+            "city": locale_match.group(1).strip(),
+            "state": locale_match.group(2)
+        }
+    }
+
+
 def scrape_events(doc=None):
-    tr = find_text_elem(doc or get_doc(), re.compile(r"^upcoming meetings$", re.I))
+    head = find_text_elem(doc or get_doc(),
+                          re.compile(r"^upcoming meetings$", re.I),
+                          {"strong"})
+    tr = head.find_parent("tr")
     schedule_row = tr.find_next_sibling("tr")
-    name_when, desc, where = schedule_row.find_all("p")[0:3]
-
-    name, day, timerange = [d for d in name_when.descendants if isinstance(d, NavigableString)]
-
-    day = datetime.strptime(day.strip(), "%B %d, %Y")
-
-    (start_time, end_time) = time_match(timerange)
-
-    start_datetime = TIMEZONE.localize(datetime.combine(day, start_time))
-    end_datetime = TIMEZONE.localize(datetime.combine(day, end_time))
-    duration = (end_datetime - start_datetime).total_seconds()
-    hours = int(duration/3600)
-    minutes = int(duration%3600/60)
-
-    place_name, address = [d for d in where.descendants if isinstance(d, NavigableString)]
-    street_address, city, state = address.rsplit(",", 2)
+    event_elements = schedule_row.find_all("p")
 
     return [
-        {
-            "title": name,
-            "description": desc.text.strip(),
-            "start": start_datetime.isoformat(),
-            "duration": f"{hours:0>2}:{minutes:0>2}",
-            "region_name": "Somerville, MA",
-            "address": {
-                "name": place_name.strip(),
-                "street_address": street_address.strip(),
-                "city": city.strip(),
-                "state": state.strip()
-            }
-        }
+        process_event(event_element) for event_element in event_elements
     ]
 
 
