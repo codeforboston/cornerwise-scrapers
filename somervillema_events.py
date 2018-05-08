c3f03167438b4cd94a7bf3960b55bba5bea58681
Brian Sanders <bdougsand+cfb@gmail.com>
2018-05-08 16:26:42 -0400

    PB/ZBA event scraping, demonstrates use of scraper utils

--- somervillema_events.py
+++ somervillema_events.py
@@ -1,92 +1,59 @@
 import os
-from urllib import request, parse
+import re
+from urllib import parse
 
-import bs4
 
 
 URL = "https://www.somervillema.gov/events?field_event_department_target_id=133&field_event_type_value=All&field_city_event_value=1"
+from scrape_utils import dict_from_selectors, date, text, text_contains
+
+import bs4
+import requests
 
 
-# def make_page_lister(tag, css_classes, attrs):
-#     def match_items(elt):
-def apply_instruction(elt, instruction):
-    if isinstance(instruction, str):
-        return elt.select_one(instruction)
 
-    if isinstance(instruction, list):
-        return elt.select(instruction[0])
-
-    if isinstance(instruction, dict):
-        return dict_from_selectors(elt, instruction)
-
-    if isinstance(instruction, tuple):
-        (selector, *fns) = instruction
-        result = apply_instruction(elt, selector)
-        if isinstance(result, list):
-            for fn in fns:
-                result = map(fn, result)
-        else:
-            for fn in fns:
-                result = fn(result)
-        return result
+URL = "https://www.somervillema.gov/event-documents"
 
 
-def dict_from_selectors(elt, selectors):
-    result = {}
-    for prop, instruction in selectors.items():
-        applied = apply_instruction(elt, instruction)
-        if isinstance(applied, map):
-            result[prop] = list(applied)
-        elif isinstance(applied, bs4.Tag):
-            result[prop] = applied.text
-        else:
-            result[prop] = applied
-    return result
 
 
-def text(elt):
-    return elt.text
 
 
-def text_children(elt):
-    pieces = (child.strip() for child in elt.children if isinstance(child, str))
-    return list(filter(None, pieces))
 
 
-def attr(attrname):
-    return lambda elt: elt.attrs[attrname]
+
+# Web scraping
+def get_page(url):
+    return bs4.BeautifulSoup(requests.get(url).content, "html.parser")
 
 
 def get_event_details(event):
     return dict_from_selectors(event, {
-        "title": "h3",
-        "url": ("h3 a", attr("href")),
-        "date": (".article-content--event-time span", attr("content")),
-        "description": ([".views-field-body p"], text),
-        "image": (".field-content a img", attr("src"))
+        "location": ".calendar-content__event-location",
+        "description": (".main-content .field-type-text-with-summary", ["p"], text),
+        "departments": ([".field-name-field-event-department li"], text),
+        "image": (".field-content a img", attr("src")),
+        "cost": ".calendar-content__price",
+        "accessibility": ".calendar-content__accessibility-text"
     })
 
 
-def get_event_page_details(event):
-    return dict_from_selectors(event, {
-        "location": (".calendar-content__event-location", text_children),
-        "full_description": (".field-name-body", text)
+def get_items(doc, url_base=URL, match=r"Planning Board|Zoning Board of Appeals"):
+    def make_absolute(url):
+        return parse.urljoin(url_base, url)
+
+    match = re.compile(match)
+
+    return dict_from_selectors(doc, {
+        "events": ([".view-event-documents tbody tr", (".views-field-field-event-doc-event", text, match)],
+                   {"title": (".views-field-field-event-doc-event", text, str.strip),
+                    "url": (".views-field-field-event-doc-event a", attr("href"), make_absolute,
+                            get_page, get_event_details),
+                    "date": (".date-display-single", attr("content"), date),
+                    "agenda": ("span.file a", attr("href"), make_absolute)})
     })
-
-
-def get_full_details(event_url):
-    url = parse.urljoin(URL, event_url)
-    with request.urlopen(url) as event_page:
-        return get_event_page_details(bs4.BeautifulSoup(event_page.read(), "html.parser"))
-
-
-def get_items(doc):
-    for event in doc.select("article"):
-        details = get_event_details(event)
-        details.update(get_full_details(details["url"]))
-        yield details
 
 
 def get_events(url=URL):
-    with request.urlopen(url) as events_page:
-        return get_items(bs4.BeautifulSoup(events_page.read(), "html.parser"))
+    response = requests.get(url)
+    return get_items(bs4.BeautifulSoup(response.content, "html.parser"))
