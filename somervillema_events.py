15318584d62f9bc4db3eab6425a1fbf41777eb19
Brian Sanders <bdougsand+cfb@gmail.com>
2018-05-09 13:17:28 -0400

    Scrape Agenda PDFs for case numbers

--- somervillema_events.py
+++ somervillema_events.py
@@ -1,25 +1,48 @@
+from datetime import datetime, timedelta
+import io
+from itertools import takewhile
 import os
 import re
 from urllib import parse
 
-
-
-URL = "https://www.somervillema.gov/events?field_event_department_target_id=133&field_event_type_value=All&field_city_event_value=1"
-from scrape_utils import dict_from_selectors, date, text, text_contains
+from cloud import aws_lambda
+from scrape_utils import attr, scrape, date, text, text_children, text_contains
+from shared import preprocess
 
 import bs4
+import pytz
 import requests
 
-
+from PyPDF2 import PdfFileReader
 
 URL = "https://www.somervillema.gov/event-documents"
+TIMEZONE = pytz.timezone("US/Eastern")
 
 
+def file_name(url):
+    return os.path.basename(parse.urlsplit(url).path)
 
 
+# PDF Scraping
+CasePattern = r"\s?((?:[\dA-Z]+-)+(?:[\dA-Z]+))"
+PBCasePattern = r"(PB)" + CasePattern
+ZBACasePattern = r"(ZBA)" + CasePattern
 
 
+def get_pdf(url):
+    pdf_in = io.BytesIO(requests.get(url).content)
+    return PdfFileReader(pdf_in)
 
+
+def get_cases(pdf, case_pattern=PBCasePattern):
+    """Extremely crude approach to scraping cases from an agenda PDF.
+    """
+    if isinstance(pdf, str):
+        pdf = get_pdf(pdf)
+
+    for page in pdf.pages:
+        matches = re.findall(case_pattern, page.extractText().replace("\n", ""))
+        yield from [f"{auth} {case}" for auth, case in matches]
 
 
 # Web scraping
@@ -28,32 +51,82 @@ def get_page(url):
 
 
 def get_event_details(event):
-    return dict_from_selectors(event, {
-        "location": ".calendar-content__event-location",
+    details =  scrape(event, {
+        "location": (".calendar-content__event-location", lambda elt: "\n".join(text_children(elt))),
         "description": (".main-content .field-type-text-with-summary", ["p"], text),
         "departments": ([".field-name-field-event-department li"], text),
         "image": (".field-content a img", attr("src")),
         "cost": ".calendar-content__price",
         "accessibility": ".calendar-content__accessibility-text"
     })
+    details["description"] = "\n".join(details["description"])
+    return details
 
 
-def get_items(doc, url_base=URL, match=r"Planning Board|Zoning Board of Appeals"):
+def url_for_page(i):
+    return f"{URL}?page={i}"
+
+
+def get_pages(make_url=url_for_page):
+    """Lazily grabs event pages from the Somerville events site. Consumers should
+    also be lazily evaluated, or else have some terminating condition;
+    otherwise, this will bombard somervillema.gov with requests.
+
+    """
+    yield from ((url, get_page(url)) for url in map(make_url, range(50)))
+
+
+def get_events(doc, url_base=URL, match=r"Planning Board|Zoning Board of Appeals"):
     def make_absolute(url):
         return parse.urljoin(url_base, url)
 
+    def remove_prefix(title: str):
+        return title[9:] if title.startswith("Download") else title
+
     match = re.compile(match)
 
-    return dict_from_selectors(doc, {
-        "events": ([".view-event-documents tbody tr", (".views-field-field-event-doc-event", text, match)],
+    return scrape(doc,
+                  ([".view-event-documents tbody tr", (".views-field-field-event-doc-event", text, match)],
                    {"title": (".views-field-field-event-doc-event", text, str.strip),
-                    "url": (".views-field-field-event-doc-event a", attr("href"), make_absolute,
-                            get_page, get_event_details),
-                    "date": (".date-display-single", attr("content"), date),
-                    "agenda": ("span.file a", attr("href"), make_absolute)})
-    })
+                    "url": (".views-field-field-event-doc-event a", attr("href"), make_absolute),
+                    "start": (".date-display-single", attr("content"), date),
+                    "documents": (["span.file a"], {"url": (attr("href"), make_absolute),
+                                                    "file_title": (attr("href"), file_name),
+                                                    "title": (text, remove_prefix)})
+                   }))
 
 
-def get_events(url=URL):
-    response = requests.get(url)
-    return get_items(bs4.BeautifulSoup(response.content, "html.parser"))
+def add_event_details(event):
+    details = get_event_details(get_page(event["url"]))
+    event.update(details)
+
+    for doc in event["documents"]:
+        if "Agenda" in doc["title"]:
+            pattern = PBCasePattern if "Planning Board" in event["title"] else ZBACasePattern
+            event["cases"] = list(get_cases(doc["url"], pattern))
+            break
+
+    event["region_name"] = "Somerville, MA"
+    return event
+
+
+def get_page_events(url, page):
+    return map(add_event_details, get_events(page, url))
+
+
+def get_all_events():
+    for url, page in get_pages():
+        yield from get_page_events(url, page)
+
+
+def get_events_since(when: datetime):
+    if not when.tzinfo:
+        when = TIMEZONE.localize(when)
+
+    yield from takewhile(lambda event: event["start"] > when, get_all_events())
+
+
+@aws_lambda
+@preprocess(TIMEZONE, timedelta(days=7))
+def run(since):
+    return {"events": list(get_events_since(since))}
