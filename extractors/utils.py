fe61c88a7b73a33c1e95ae59c096f60772d8cc26
Brian Sanders <bdougsand+cfb@gmail.com>
2020-11-29 18:10:09 -0500

    Supply margins to pdftotext

--- extractors/utils.py
+++ extractors/utils.py
@@ -1,10 +1,19 @@
 from os import path
+import re
 import subprocess
 
 
 LayerDir = "/opt/bin"
 PdfInfo = path.join(LayerDir, "pdfinfo")
 PdfToText = path.join(LayerDir, "pdftotext")
+
+
+DefaultExtractOpts = {
+    "marginb": 65,
+    "margint": 65,
+    "marginl": 65,
+    "marginr": 65,
+}
 
 class pushback_iter(object):
     """An iterator that implements a pushback() method, allowing values to
@@ -28,14 +37,42 @@ be added back to the 'stack' after consideration.
             return self.pushed.pop()
         return next(self.iterable)
 
+def parse_pdf_dims(dims):
+    m = re.match(r"(\d+) x (\d+) pt", dims)
+    if m:
+        return (int(m.group(1)), int(m.group(2)))
+
+    return (None, None)
 
 def pdfinfo(path):
     return dict(re.findall(
         r"([\w ]+):\s+([^\n]*)\n",
-        subprocess.check_output([PdfInfo, path])))
+        subprocess.check_output([PdfInfo, path]).decode()))
 
-def pdf_to_text(pdf_path, out_file):
-    args = [PdfToText, "-enc", "UTF-8", pdf_path]
+def pdf_dims(path):
+    info = pdfinfo(path)
+    return parse_pdf_dims(info["Page size"])
+
+def prepare_pdfextract_args(opts, dims=None):
+    if dims:
+        [x, y, b, r] = [opts.get(f"margin{k}", 0) for k in ["l", "t", "b", "r"]]
+        if x+y+b+r:
+            (w, h) = dims
+            opts.update({
+                "x": x,
+                "y": y,
+                "W": w-r-x,
+                "H": h-b-y,
+            })
+
+    return sum([[f"-{kopt}"] if v is True else [f"-{kopt}", str(v)]
+                for kopt, v in opts.items() if not kopt.startswith("margin")], [])
+
+def pdf_to_text(pdf_path, out_file, extra_opts=DefaultExtractOpts):
+    opts = { "enc": "UTF-8" }
+    opts.update(extra_opts)
+
+    args = [PdfToText, *prepare_pdfextract_args(opts)]
     kwargs = {}
     if isinstance(out_file, str):
         args.append(out_file)
